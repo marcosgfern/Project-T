8bc274002b003d025006dd3586a1033336baa965
mgfernandez3 <mgfernandez3@esei.uvigo.es>
2021-01-29 11:37:53 +0100

    Changed floor map from matrix to dictionary for memory efficiency

--- Project T/Assets/Scripts/Floor/Floor.cs
+++ Project T/Assets/Scripts/Floor/Floor.cs
@@ -6,10 +6,11 @@ public class Floor : MonoBehaviour {
 
     public GameObject roomPrefab;
     private FloorGenerator generator;
+    private Dictionary<Coordinate, Room> roomMap;
 
     private void Start() {
         this.generator = new FloorGenerator(10, this.roomPrefab, this.gameObject.transform);
-        this.generator.Generate(854353174);
+        this.roomMap = this.generator.Generate(854353174);
     }
 
     private class FloorGenerator {
@@ -18,45 +19,30 @@ public class Floor : MonoBehaviour {
         private Transform floor; 
 
         private int level;
-        private Room[,] roomMatrix;
-        private int matrixSize;
+        private Dictionary<Coordinate, Room> roomMap;
         private int numberOfRooms;
-        private RoomCoordinate firstRoom;
 
         public FloorGenerator (int level, GameObject roomPrefab, Transform floor) {
             this.level = level;
             this.roomPrefab = roomPrefab;
             this.floor = floor;
+            this.roomMap = new Dictionary<Coordinate, Room>();
         }
 
 
-        public void Generate(int seed) {
+        public Dictionary<Coordinate, Room> Generate(int seed) {
+            roomMap.Clear();
+
             Random.InitState(seed);
 
-            SetFloorSize();
-
-            this.firstRoom = new RoomCoordinate(
-                Random.Range(1, this.matrixSize - 1),
-                Random.Range(1, this.matrixSize - 1)
-            );
-
-            AddRoom(this.firstRoom);
-
-            GenerateRooms();
-        }
-
-        private void SetFloorSize() {
             this.numberOfRooms = 6 + (int)(level * 1.5) + (int)Random.Range(0, Mathf.Sqrt(level));
 
-            if (this.numberOfRooms <= 10) {
-                this.matrixSize = this.numberOfRooms;
-            } else {
-                this.matrixSize = (int)Mathf.Sqrt(this.numberOfRooms * Mathf.Sqrt(this.numberOfRooms));
-            }
-            this.roomMatrix = new Room[this.matrixSize, this.matrixSize];
+            GenerateRooms();
+
+            return this.roomMap;
         }
 
-        private void AddRoom(RoomCoordinate roomCoordinate) {
+        private void AddRoom(Coordinate roomCoordinate) {
             Vector2 roomPosition = new Vector2(
                 roomCoordinate.x * roomPrefab.GetComponent<Renderer>().bounds.size.x,
                 roomCoordinate.y * roomPrefab.GetComponent<Renderer>().bounds.size.y
@@ -66,20 +52,27 @@ public class Floor : MonoBehaviour {
             room.name = roomCoordinate.ToString();
             room.transform.parent = this.floor;
 
-            this.roomMatrix[roomCoordinate.x, roomCoordinate.y] = room.GetComponent<Room>();
+            Room roomScript = room.GetComponent<Room>();
+
+            //Set neighbours
+
+            this.roomMap.Add(roomCoordinate, roomScript);
         }
 
         private void GenerateRooms() {
-            List<RoomCoordinate> availablePlaces = AvailableNeighbours(this.firstRoom);
+            Coordinate firstRoom = new Coordinate(0, 0);
+            AddRoom(firstRoom);
+
+            List<Coordinate> availablePlaces = AvailableNeighbours(firstRoom);
 
             for (int i = 0; i < this.numberOfRooms - 1; i++) {
                 int placeIndex = Random.Range(0, availablePlaces.Count - 1);
-                RoomCoordinate newRoom = availablePlaces[placeIndex];
+                Coordinate newRoom = availablePlaces[placeIndex];
                 AddRoom(newRoom);
                 availablePlaces.RemoveAt(placeIndex);
 
-                foreach (RoomCoordinate neighbour in AvailableNeighbours(newRoom)) {
-                    if (!availablePlaces.Exists(room => room.x == neighbour.x && room.y == neighbour.y)) {
+                foreach (Coordinate neighbour in AvailableNeighbours(newRoom)) {
+                    if (!availablePlaces.Contains(neighbour)) {
                         availablePlaces.Add(neighbour);
                     }
                 }
@@ -88,10 +81,10 @@ public class Floor : MonoBehaviour {
             }
         }
 
-        private List<RoomCoordinate> AvailableNeighbours(RoomCoordinate room) {
-            List<RoomCoordinate> availableNeighbours = new List<RoomCoordinate>();
+        private List<Coordinate> AvailableNeighbours(Coordinate room) {
+            List<Coordinate> availableNeighbours = new List<Coordinate>();
 
-            foreach (RoomCoordinate neighbour in room.Neighbours()) {
+            foreach (Coordinate neighbour in room.GetNeighbours()) {
                 if (IsAvailable(neighbour)) {
                     availableNeighbours.Add(neighbour);
                 }
@@ -100,77 +93,70 @@ public class Floor : MonoBehaviour {
             return availableNeighbours;
         }
 
-        private bool IsUsable(RoomCoordinate room) {
+        private bool IsUsable(Coordinate room) {
             int numberOfNeighbours = 0;
 
-            foreach (RoomCoordinate neighbour in room.Neighbours()) {
-                if (!neighbour.IsOutOfBounds(this.roomMatrix.GetLength(0))) {
-                    if (this.roomMatrix[neighbour.x, neighbour.y] != null) {
-                        numberOfNeighbours++;
-                    }
+            foreach (Coordinate neighbour in room.GetNeighbours()) {
+                if (this.roomMap.ContainsKey(neighbour)) {
+                    numberOfNeighbours++;
                 }
             }
 
             return numberOfNeighbours == 1;
         }
 
-        private bool IsAvailable(RoomCoordinate room) {
-            if (room.IsOutOfBounds(this.roomMatrix.GetLength(0))) {
+        private bool IsAvailable(Coordinate room) {
+                return !this.roomMap.ContainsKey(room);
+        }
+    }
+
+    private class Coordinate {
+        public int x, y;
+
+        public Coordinate(int x, int y) {
+            this.x = x;
+            this.y = y;
+        }
+
+        //Change list to array
+        public List<Coordinate> GetNeighbours() {
+            List<Coordinate> neighbours = new List<Coordinate>();
+
+            neighbours.Add(new Coordinate(this.x, this.y + 1));
+            neighbours.Add(new Coordinate(this.x + 1, this.y));
+            neighbours.Add(new Coordinate(this.x, this.y - 1));
+            neighbours.Add(new Coordinate(this.x - 1, this.y));
+
+            return neighbours;
+        }
+
+        //To remove
+        public bool IsOutOfBounds(int bounds) {
+            return (this.x < 0 ||
+                this.x >= bounds ||
+                this.y < 0 ||
+                this.y >= bounds);
+        }
+
+        override public string ToString() {
+            return "[" + this.x + "," + this.y + "]";
+        }
+
+        public override bool Equals(object obj) {
+            if(obj == null) {
                 return false;
-            } else {
-                return this.roomMatrix[room.x, room.y] == null;
             }
+
+            if(!(obj is Coordinate)) {
+                return false;
+            }
+
+            return this.x == ((Coordinate)obj).x 
+                && this.y == ((Coordinate)obj).y;
         }
 
-        public GameObject FirstRoom() {
-            return this.roomMatrix[this.firstRoom.x, this.firstRoom.y].gameObject;
-        }
-
-        private class RoomCoordinate {
-            public int x, y;
-
-            public RoomCoordinate(int x, int y) {
-                this.x = x;
-                this.y = y;
-            }
-
-            public RoomCoordinate Up() {
-                return new RoomCoordinate(this.x, this.y + 1);
-            }
-
-            public RoomCoordinate Right() {
-                return new RoomCoordinate(this.x + 1, this.y);
-            }
-
-            public RoomCoordinate Down() {
-                return new RoomCoordinate(this.x, this.y - 1);
-            }
-
-            public RoomCoordinate Left() {
-                return new RoomCoordinate(this.x - 1, this.y);
-            }
-
-            public List<RoomCoordinate> Neighbours() {
-                List<RoomCoordinate> neighbours = new List<RoomCoordinate>();
-
-                neighbours.Add(Up());
-                neighbours.Add(Right());
-                neighbours.Add(Down());
-                neighbours.Add(Left());
-
-                return neighbours;
-            }
-
-            public bool IsOutOfBounds(int bounds) {
-                return (this.x < 0 ||
-                    this.x >= bounds ||
-                    this.y < 0 ||
-                    this.y >= bounds);
-            }
-
-            override public string ToString() {
-                return "[" + this.x + "," + this.y + "]";
-            }
+        public override int GetHashCode() {
+            return (this.x + "," + this.y).GetHashCode();
         }
     }
 
