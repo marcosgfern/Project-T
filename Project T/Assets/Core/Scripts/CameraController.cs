a82f883b8ea71f24e775eddcd32748ca07c334c0
marcosgfern <marcosgfern@gmail.com>
2024-08-19 12:03:44 +0200

    Started screenshake when hit (wip)

--- Project T/Assets/Core/Scripts/CameraController.cs
+++ Project T/Assets/Core/Scripts/CameraController.cs
@@ -1,4 +1,5 @@
-﻿using System.Collections;
+﻿using DG.Tweening;
+using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
@@ -6,53 +7,106 @@ using UnityEngine;
  * Adjusts size an position to adapt to different screen ratios and sizes.
  * Moves camera to corresponding room.
  */
-public class CameraController : MonoBehaviour {
+public class CameraController : MonoBehaviour 
+{
+    [Header("Shaking parameters")]
+    [SerializeField] private float shakeDuration;
+    [SerializeField] private float shakeStrength;
+    [SerializeField] private int shakeVibrato;
+    [SerializeField] private float shakeRandomness;
+    [SerializeField] private bool shakeFadeOut;
+    [SerializeField] private ShakeRandomnessMode shakeRandomnessMode;
+    [SerializeField] private float timeFreeze;
 
-    public RectTransform upperUI, lowerUI, canvas;
+    [Header("References")]
+    [SerializeField] private RectTransform upperUI;
+    [SerializeField] private RectTransform lowerUI;
+    [SerializeField] private RectTransform canvas;
+    
     private float shift;
     private float panningDuration = 0.2f;
 
-    void Awake() {
+    void Awake()
+    {
         AdjustOrtographicSize();
         CalculateCameraYShift();
     }
 
-    public float GetPanningDuration() {
+    private void Start()
+    {
+        StartCoroutine(ShakeRepeater());
+    }
+
+    public float GetPanningDuration()
+    {
         return this.panningDuration;
     }
 
     /* Calculates and sets the Y shift of the camera, necessary to center rooms in the space between lower and upper UI panels. */
-    public void CalculateCameraYShift() {
+    public void CalculateCameraYShift()
+    {
        float screenShift = (lowerUI.rect.height - upperUI.rect.height) / 2;
         this.shift = (Camera.main.ScreenToWorldPoint(new Vector3(0, screenShift, 0)) - Camera.main.ScreenToWorldPoint(Vector3.zero)).y;
     }
 
     /* Adjusts orthographic size to fix the width ratio in different screen sizes (Unity fixes height by default) */
-    private void AdjustOrtographicSize() {
+    private void AdjustOrtographicSize()
+    {
         SpriteRenderer aspectRatioModel = GetComponentInChildren<SpriteRenderer>();
 
         float screenRatio = (float)Screen.width / (float)Screen.height;
         float targetRatio = aspectRatioModel.bounds.size.x / aspectRatioModel.bounds.size.y;
 
-        if (screenRatio >= targetRatio) {
+        if (screenRatio >= targetRatio)
+        {
             Camera.main.orthographicSize = aspectRatioModel.bounds.size.y / 2;
-        } else {
+        } 
+        else
+        {
             Camera.main.orthographicSize = aspectRatioModel.bounds.size.y / 2 * (targetRatio / screenRatio);
         }
     }
 
     /* Moves the camera to @room */
-    public void MoveToRoom(Transform room) {
+    public void MoveToRoom(Transform room) 
+    {
         StartCoroutine(Panning(new Vector3(room.position.x, room.position.y - this.shift, this.transform.position.z), this.panningDuration));
     }
 
     /* Coroutine: Moves camera from the current position to @targetPosition in @duration seconds */
-    private IEnumerator Panning(Vector3 targetPosition, float duration) {
+    private IEnumerator Panning(Vector3 targetPosition, float duration)
+    {
         Vector3 startingPosition = this.transform.position;
-        for (float t = 0f; t < duration; t += Time.deltaTime) {
+        for (float t = 0f; t < duration; t += Time.deltaTime)
+        {
             this.transform.position = Vector3.Lerp(startingPosition, targetPosition, t/duration);
             yield return Time.deltaTime;
         }
         this.transform.position = targetPosition;
+    }
+
+    public void Shake()
+    {
+        Camera.main.DOShakePosition(
+            shakeDuration,
+            shakeStrength,
+            shakeVibrato,
+            shakeRandomness,
+            shakeFadeOut,
+            shakeRandomnessMode)
+            .SetUpdate(UpdateType.Normal, true);
+    }
+
+    private IEnumerator ShakeRepeater()
+    {
+        yield return new WaitForSeconds(5);
+        while (true)
+        {
+            Time.timeScale = 0.1f;
+            Shake();
+            yield return new WaitForSeconds(timeFreeze*Time.timeScale);
+            Time.timeScale = 1;
+            yield return new WaitForSeconds(3-timeFreeze);
+        }
     }
 }
